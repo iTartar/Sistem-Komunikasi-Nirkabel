d291ee0443001951a4006265f7a16ca27cc80f91
Aqil <121059067+iTartar@users.noreply.github.com>
2025-11-15 12:38:03 +0700

    Add files via upload
    get uploaded

---
+++ Prak12/DHT11/DHT11.ino
@@ -0,0 +1,31 @@
+#include "DHT.h"
+
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Deteksi Suhu dan Kelembaban");
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);
+  
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+  
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Sensor tidak terbaca!");
+    return;
+  }
+  
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" *C");
+}
